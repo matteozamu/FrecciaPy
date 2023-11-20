a154caf3c2f6bbed11261d6ef31435c88fdc6a72
matteozamu <matteoz2001@gmail.com>
2023-11-20 19:07:38 +0100

    fix url open and delay print

--- main.py
+++ main.py
@@ -12,23 +12,27 @@ url = 'https://d6o.portalefrecce.it/PortaleFrecce/infoViaggioActionJson'
 
 while True:
   try:
-    response = urllib3.request.urlopen(url)
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
   except Exception as e:
     # print(f"An error occurred: {e}")
     print("You are not connected to the Frecciarossa WiFi :(\nPlease, connect to it")
     sys.exit(-1)
-      
-  json_string = json.loads((response.read()).decode('utf-8'))
 
-  json_response = json.loads(json_string)
+  try:
+    json_response = json.loads(response.data)
+  except Exception as e:
+    print("An error occured reading the data :(")
+    sys.exit(-1)
 
   if json_response['isGpsValid'] == True:
     ui.gps['speed'] = json_response['speed']
     ui.trackLine = json_response['trackline']
     
-    match = re.search(r'\+(\d+)min', json_response['delay2'])
-    if match:
-      ui.gps['delay'] = "+" + match.group(1) + " min"
+    pattern = r'\d+'
+    matches = re.findall(pattern, json_response['delay2'])
+    if matches:
+      ui.gps['delay'] = "+" + matches[0] + " min"
     else:
       ui.gps['delay'] = "On Time"
 
